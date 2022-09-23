34bded0d5ba24ec91526b1c5df0478a241971667
ITMK <77116913+ITMK@users.noreply.github.com>
2022-09-23 10:39:09 +0200

    Create vid.py

---
+++ learning_resources/MT_priming/vid.py
@@ -0,0 +1,5 @@
+%%HTML
+<div align="middle">
+<video width="80%" controls>
+      <source src="C:\Users\janic\Desktop\TH\WHK - WMA\DataLitMT\Learning Resources\MT Priming\MT-priming-effect-video.mp4" type="video/mp4">
+</video></div>
