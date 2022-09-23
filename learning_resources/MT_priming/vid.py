84b7851a11c019489bde6badd8dbcb17400e7171
ITMK <77116913+ITMK@users.noreply.github.com>
2022-09-23 10:39:18 +0200

    Delete vid.py

--- learning_resources/MT_priming/vid.py
+++
@@ -1,5 +0,0 @@
-%%HTML
-<div align="middle">
-<video width="80%" controls>
-      <source src="C:\Users\janic\Desktop\TH\WHK - WMA\DataLitMT\Learning Resources\MT Priming\MT-priming-effect-video.mp4" type="video/mp4">
-</video></div>
